57c657cfbf266db22e85904602eae093c83e9cd1
Shreyas Deo <93237883+deoshreyas@users.noreply.github.com>
2024-09-01 03:09:26 +0530

    Final update to robot.ino

--- robot/robot.ino
+++ robot/robot.ino
@@ -1,6 +1,5 @@
-// #include <AFMotor.h> 
+#include <AFMotor.h> 
 #include <NewPing.h>  
-#include <Servo.h>  
 
 // Define sensor constants 
 unsigned int front_dst = 0;
@@ -15,61 +14,58 @@ unsigned int right_dst = 0;
 NewPing front_sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
 // Create motor objects
-// AF_DCMotor Motor1(1,MOTOR12_1KHZ);
-// AF_DCMotor Motor2(2,MOTOR12_1KHZ);
-// AF_DCMotor Motor3(3,MOTOR34_1KHZ);
-// AF_DCMotor Motor4(4,MOTOR34_1KHZ);
-
-// Servo Motor 
-Servo myservo;
-int pos = 0;
-
-// SERVO MOTOR DOES NOT WORK (TODO: FIGURE IT OUT OR REMOVE)
+AF_DCMotor Motor1(1, MOTOR12_1KHZ);
+AF_DCMotor Motor2(2, MOTOR12_1KHZ);
+AF_DCMotor Motor3(3, MOTOR34_1KHZ);
+AF_DCMotor Motor4(4, MOTOR34_1KHZ);
 
 void setup() {
-  Serial.begin(9600);
-  myservo.attach(10);
   pinMode(LEFT_INFRA, INPUT); // left infrared sensor
   pinMode(RIGHT_INFRA, INPUT); // right infrared sensor
-  {
-  for(pos = 90; pos <= 180; pos += 1){    
-    myservo.write(pos);                   
-    delay(15);                            
-    } 
-  for(pos = 180; pos >= 0; pos-= 1) {     
-    myservo.write(pos);                   
-    delay(15);                            
-    }
-  for(pos = 0; pos<=90; pos += 1) {       
-    myservo.write(pos);                   
-    delay(15);                            
-    }
-  }
 }
 
 void loop() {   
-  // front_dst = front_sonar.ping_cm();
-  // Serial.print("Front dst: ");
-  // Serial.println(front_dst);
-  // left_dst = digitalRead(LEFT_INFRA);
-  // right_dst = digitalRead(RIGHT_INFRA);
-  // Serial.print("Left: ");
-  // Serial.println(left_dst);
-  // Serial.print("Right: ");
-  // Serial.println(right_dst);
+  front_dst = front_sonar.ping_cm();
+  left_dst = digitalRead(LEFT_INFRA);
+  right_dst = digitalRead(RIGHT_INFRA);
 
-  // delay(100);
-  // // TODO: Increase speed (testing for now)
-  // Motor1.setSpeed(130);  
-  // Motor1.run(FORWARD);   
-  // Motor2.setSpeed(130);  
-  // Motor2.run(FORWARD);  
-  // Motor3.setSpeed(130); 
-  // Motor3.run(FORWARD);  
-  // Motor4.setSpeed(130); 
-  // Motor4.run(FORWARD); 
+  delay(50);
 
-
+  if (front_dst>1 && front_dst<15) { // If object in front, go forward
+    Motor1.setSpeed(200);  
+    Motor1.run(BACKWARD);   
+    Motor2.setSpeed(200);  
+    Motor2.run(BACKWARD);  
+    Motor3.setSpeed(200); 
+    Motor3.run(BACKWARD);  
+    Motor4.setSpeed(200); 
+    Motor4.run(BACKWARD); 
+  } else if (right_dst==0 && left_dst==1) { // If object on the right, turn right
+    Motor1.setSpeed(200);  
+    Motor1.run(BACKWARD);   
+    Motor2.setSpeed(200);  
+    Motor2.run(BACKWARD);  
+    Motor3.setSpeed(200); 
+    Motor3.run(FORWARD);  
+    Motor4.setSpeed(200); 
+    Motor4.run(FORWARD); 
+  } else if (left_dst==0 && right_dst==1) { // If object on the left, turn left
+    Motor1.setSpeed(200);  
+    Motor1.run(FORWARD);   
+    Motor2.setSpeed(200);  
+    Motor2.run(FORWARD);  
+    Motor3.setSpeed(200); 
+    Motor3.run(BACKWARD);  
+    Motor4.setSpeed(200); 
+    Motor4.run(BACKWARD); 
+  } else if (front_dst > 15) { // If none then stop
+    Motor1.setSpeed(0);    
+    Motor1.run(RELEASE);   
+    Motor2.setSpeed(0);    
+    Motor2.run(RELEASE);   
+    Motor3.setSpeed(0);    
+    Motor3.run(RELEASE);   
+    Motor4.setSpeed(0);    
+    Motor4.run(RELEASE);   
+  }
 }
-
-// When object detected (infrared reading) : 0
